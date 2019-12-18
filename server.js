7c7207845dcda590b390268318997a173c92add3
martucazpo <martucazpo@gmail.com>
2019-12-18 13:58:24 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const routes = require('./routes')
+const mongoose = require('mongoose')
+const db = mongoose.connection
+const PORT = process.env.PORT || 3000
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use(routes)
+
+mongoose.connect(process.env.DATABASE_URL, {
+useNewUrlParser: true,
+useUnifiedTopology: true
+})
+db.on('error', error => console.error(error))
+db.once('open', () => console.log( 'mongoose is on the loose' ))
+
+app.listen(PORT)
