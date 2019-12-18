9d863fffc1d4df83f4e41d9a70c4642b247dd680
martucazpo <martucazpo@gmail.com>
2019-12-18 14:46:40 -0700

    changed /

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-app.use('/', routes)
+app.use(routes)
 
 mongoose.connect(process.env.DATABASE_URL || MONGODB_URI, {
 useNewUrlParser: true,
