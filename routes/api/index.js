f99a6c9056ab67d15e15f09c4280b85e3e1e5638
martucazpo <martucazpo@gmail.com>
2019-12-19 16:24:35 -0700

    book routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const authorRoutes = require('./authors');
+const bookRoutes = require('./books')
 
 // Book routes
 router.use("/authors", authorRoutes);
+router.use("/books", bookRoutes);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 
 const router = require('express').Router()
 const apiRoutes = require('./api')
+const Book = require('../models/book')
 
 router.use('/', apiRoutes)
 
-router.get('/', (req, res) => {
-    res.render('index')
+router.get('/', async (req, res) => {
+    let book = []
+    try{
+        books = await Book.find().sort({ createdAt: 'desc' }).limit(10).exec()
+    } catch {
+        books = []
+    }
+    res.render('index', { books: books })
 })
 
 module.exports = router
