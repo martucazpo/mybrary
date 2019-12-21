471d48786b83ed8a5e7f4bd40e05014f240084a5
martucazpo <martucazpo@gmail.com>
2019-12-20 18:08:53 -0700

    finished author routes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const routes = require('./routes')
 const mongoose = require('mongoose')
 const db = mongoose.connection
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
@@ -17,6 +18,7 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+app.use(methodOverride('_method'))
 app.use(routes)
 
 mongoose.connect(process.env.DATABASE_URL || process.env.MONGODB_URI, {
